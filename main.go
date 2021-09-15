aa22b47b69c6720520a3e8f042369bee19fcf7df
Enderman112 <75347659+Enderman112@users.noreply.github.com>
2021-09-15 21:33:02 +0800

    Update main.go

--- main.go
+++ main.go
@@ -73,7 +73,7 @@ func main() {
 		"============================================================\n",
 	) // 启动打印
 	zero.Run(zero.Config{
-		NickName:      []string{"bot"},
+		NickName:      []string{"罗伯特"},
 		CommandPrefix: "/",
 
 		// SuperUsers 某些功能需要主人权限，可通过以下两种方式修改
